5e67daa7744f2b95316d12567b3bf6c77e19f2e0
RaoulG1406 <raoulgoorden@gmail.com>
2020-01-08 10:08:51 +0100

    Add project files.

---
+++ BotlerMain/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BotlerMain.Models;
+
+namespace BotlerMain.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
