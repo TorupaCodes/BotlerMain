c34c4732f046bcd747826461675e29df852ce93c
RaoulG1406 <raoulgoorden@gmail.com>
2020-01-08 14:35:56 +0100

    Recepten toegevoegd

---
+++ BotlerMain/Views/MyRecipePageDetail.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BotlerMain.Views
+{
+    public partial class MyRecipePageDetail : ContentPage
+    {
+        public MyRecipePageDetail(string Name, string Ingredients, string source)
+        {
+            InitializeComponent();
+
+            MyItemNameShow.Text = Name;
+            MyIngrediantItemShow.Text = Ingredients;
+            MyImageCall.Source = new UriImageSource()
+
+            {
+                Uri = new Uri(source)
+            };
+
+        }
+    }
+}
